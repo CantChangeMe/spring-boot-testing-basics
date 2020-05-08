61d55b9c173470966649bd82547ebb4b1fe2169d
Vivek Kumar UAE <Vivek.Kumar@XEBIAINDIA.LOCAL>
2020-05-09 03:33:26 +0530

    Initial commit

---
+++ src/main/java/com/vivek/spring/boot/testing/springboottestingbasics/SpringBootTestingBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.spring.boot.testing.springboottestingbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTestingBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/spring/boot/testing/springboottestingbasics/SpringBootTestingBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.spring.boot.testing.springboottestingbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
